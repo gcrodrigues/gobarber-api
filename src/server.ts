7d332c265b592952dfad9e58847a76cde2aa54e9
Gustavo Carvalho <grodrigues.modalgr@ext.valid.com>
2021-05-02 02:13:39 -0300

    feat: starting gobarber project

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
+import 'reflect-metadata';
+
 import express from 'express';
+import routes from './routes';
+
+import './database';
 
 const app = express();
 
 app.use(express.json());
-
-app.get('/', (request, response) => {
-  return response.json({message: 'Hello world'})
-})
+app.use(routes);
 
 app.listen(3333, () => {
-  console.log('Server running at http://localhost:3333')
+  console.log('Server running at http://localhost:3333');
 });
