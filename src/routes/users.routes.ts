f95994e5ab6f69ce0554571db8578f18188ae190
Gustavo Carvalho <grodrigues.modalgr@ext.valid.com>
2021-05-02 02:16:46 -0300

    feat: create users development

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,52 @@
+import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+
+import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
+const upload = multer(uploadConfig);
+
+const usersRouter = Router();
+
+usersRouter.post('/', async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({ name, email, password });
+
+    //@ts-expect-error
+    delete user.password;
+
+    return res.json(user);
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
+  }
+});
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (req, res) => {
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: req.user.id,
+        avatarFilename: req.file.filename,
+      });
+
+      // @ts-expect-error
+      delete user.password;
+
+      return res.json(user);
+    } catch (err) {
+      return res.status(400).json({ error: err.message });
+    }
+  },
+);
+
+export default usersRouter;
