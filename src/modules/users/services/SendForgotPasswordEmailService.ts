b0fc6bef443fa2eb6fbac9d95054eb3a70859d77
Gustavo Carvalho <grodrigues.modalgr@ext.valid.com>
2021-06-09 23:40:02 -0300

    feat: forgot email and reset password development

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,57 @@
+import { injectable, inject } from 'tsyringe';
+import path from 'path';
+
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import AppError from '@shared/errors/AppError';
+import IUsersRepository from '../repositories/IUsersRepository';
+import IUserTokensRepository from '../repositories/IUserTokensRepository';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository') private usersRepository: IUsersRepository,
+    @inject('MailProvider') private mailProvider: IMailProvider,
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
+  ) {}
+
+  public async execute({ email }: IRequest): Promise<void> {
+    const checkUserExists = await this.usersRepository.findByEmail(email);
+
+    if (!checkUserExists) {
+      throw new AppError('User does not exists', 401);
+    }
+
+    const { token } = await this.userTokensRepository.generate(
+      checkUserExists.id,
+    );
+
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
+    );
+
+    await this.mailProvider.sendMail({
+      to: {
+        name: checkUserExists.name,
+        email: checkUserExists.email,
+      },
+      subject: '[GoBarber] Recuperação de senha',
+      templateData: {
+        file: forgotPasswordTemplate,
+        variables: {
+          name: checkUserExists.name,
+          link: `http://localhost:3000/reset_password?token=${token}`,
+        },
+      },
+    });
+  }
+}
+
+export default SendForgotPasswordEmailService;
