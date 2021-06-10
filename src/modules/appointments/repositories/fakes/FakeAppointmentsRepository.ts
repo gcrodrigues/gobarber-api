021510faeae890eca6b62ec9d3561ee34a80568b
Gustavo Carvalho <grodrigues.modalgr@ext.valid.com>
2021-06-09 23:38:50 -0300

    feat: fake appointments repository

---
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -0,0 +1,34 @@
+import { v4 } from 'uuid';
+import { isEqual } from 'date-fns';
+
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import ICreateAppointmenteDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+
+class AppointmentsRepository implements IAppointmentsRepository {
+  private appointments: Appointment[] = [];
+
+  public async create({
+    provider_id,
+    date,
+  }: ICreateAppointmenteDTO): Promise<Appointment> {
+    const appointment = new Appointment();
+
+    Object.assign(appointment, { id: v4(), date, provider_id });
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const findAppointment = this.appointments.find(appointment =>
+      isEqual(appointment.date, date),
+    );
+
+    return findAppointment;
+  }
+}
+
+export default AppointmentsRepository;
